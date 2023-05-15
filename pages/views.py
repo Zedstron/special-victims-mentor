0e4021258ed91611d464af01a95015d3b1eba1e7
Zain Ul Abidin <zain26134@gmail.com>
2023-05-15 22:31:19 +0500

    Optimized files, removed garbage media data

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return render(request, 'pages/index.html')
 
 def about(request):
     return render(request, 'pages/about.html')
+
+def upload(request):
+    context = { "status": False }
+    
+    try:
+        if request.method == 'POST' and request.FILES['file']:
+            myfile = request.FILES['file']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+        
+            context["url"] = fs.url(filename)
+            context["message"] = "File has been uploaded successfully"
+            context["status"] = True
+        else:
+            context["message"] = "Either no file received or invalid request method"
+    except Exception as e:
+        context["message"] = "Exception occured {}".format(str(e))
+
+    return render(request, 'pages/upload.html', context)
